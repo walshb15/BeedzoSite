a79b43db4f447656a7baf2ae2db1f9e9a6181ee2
Brendan Walsh <walshb15@msu.edu>
2020-07-21 23:53:37 -0400

    Adding the initial work I already started
    Nothing too much yet. Just some basic buttons for an animated sidebar.

---
+++ beedzo.js
@@ -0,0 +1,27 @@
+{
+    function closeSidebar() {
+        let side = document.getElementById("sideSections");
+        side.style.width = "0%";
+        side.style.borderColor = "transparent";
+        let items = document.getElementsByClassName("sidebarItems");
+        //Set the items in the sidebar to be transparent
+        for (i = 0; i < items.length; ++i)
+        {
+            items[i].style.opacity = "0";
+        }
+        document.getElementById("menuButton").style.opacity = "100%";
+    }
+
+    function openSidebar() {
+        let side = document.getElementById("sideSections");
+        side.style.width = "20%";
+        side.style.borderColor = "lightslategray";
+        let items = document.getElementsByClassName("sidebarItems");
+        //Set the items in the sidebar to be visible
+        for (i = 0; i < items.length; ++i)
+        {
+            items[i].style.opacity = "1";
+        }
+        document.getElementById("menuButton").style.opacity = "0%";
+    }
+}

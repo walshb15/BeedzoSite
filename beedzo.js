5d389e54390baa01450a7c848c90006990e681a6
Brendan Walsh <walshb15@msu.edu>
2020-07-24 18:31:33 -0400

    Added background image/Welcome
    Making the site a bit prettier. For now I'm working on getting the main welcome page to look nice.

--- beedzo.js
+++ beedzo.js
@@ -3,6 +3,7 @@
         let side = document.getElementById("sideSections");
         side.style.width = "0%";
         side.style.borderColor = "transparent";
+        side.style.backgroundColor = 'transparent';
         let items = document.getElementsByClassName("sidebarItems");
         //Set the items in the sidebar to be transparent
         for (i = 0; i < items.length; ++i)
@@ -16,6 +17,7 @@
         let side = document.getElementById("sideSections");
         side.style.width = "20%";
         side.style.borderColor = "lightslategray";
+        side.style.backgroundColor = "black";
         let items = document.getElementsByClassName("sidebarItems");
         //Set the items in the sidebar to be visible
         for (i = 0; i < items.length; ++i)
